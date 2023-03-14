30dd55fdf2e2a7beff989079ad1b2c5305b54187
Dmitriy Ovchinnikov <dlcerberos@gmail.com>
2023-03-14 14:27:19 +0300

    ads

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,18 +1,18 @@
 import { PageIndex } from './components/PageIndex/PageIndex';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { SingleItemCard } from './components/SingleItemCard/SingleItemCard';
 import { PageMain } from './components/PageMain/PageMain';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<PageMain />} />
         <Route path="/index" element={<PageIndex />} />
         <Route path="/index/:id" element={<SingleItemCard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
